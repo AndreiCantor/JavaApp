6ff92cb3a22ae34f410ec520aa9e758298beaf74
Radu Ciurca <raduciurca@gmail.com>
2025-12-01 11:28:47 +0200

    swagger

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -1,9 +1,50 @@
 package com.example.demo;
 
+import com.example.demo.resource.AuthorResource;
+import com.example.demo.resource.BookResource;
+import com.example.demo.resource.UserResource;
+import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+
 @ApplicationPath("/api")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library Web Service",
+                version = "1.0",
+                description = "REST API pentru gestionarea cărților, autorilor, utilizatorilor și împrumuturilor."
+        )
+        ,
+        servers = {
+                @Server(
+                        url = "/demo_war_exploded",   // 🔥 context path + /api
+                        description = "Tomcat local"
+                )
+        }
+)
 public class HelloApplication extends Application {
 
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+
+        // Inregistrarea resurselor
+        resources.add(BookResource.class);
+        resources.add(AuthorResource.class);
+        resources.add(UserResource.class);
+
+        // Inregistrare resursa Swagger/OpenAPI
+        resources.add(OpenApiResource.class);
+
+        return resources;
+    }
 }
